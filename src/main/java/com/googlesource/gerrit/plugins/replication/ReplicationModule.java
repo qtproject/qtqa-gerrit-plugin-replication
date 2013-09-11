9d8d29e97b2ad94a2684b0956a15506836e8eff3
Edwin Kempin <edwin.kempin@sap.com>
2013-09-11 16:39:05 +0200

    Support replication of project deletions
    The replication plugin can now be configured to listen to project
    deletion events and to replicate the project deletions. In the
    configuration it can be decided per remote destination whether project
    deletions should be replicated to it or not. By default project
    deletions are not replicated.
    
    Change-Id: I1207015ed361155dd0353bed86860c794cdb2517
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    (cherry picked from commit ad923e4e1e4ae3923d15a8b4eee36379423846be)
    Reviewed-by: Ismo Haataja <ismo.haataja@digia.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationModule.java
@@ -17,6 +17,7 @@ package com.googlesource.gerrit.plugins.replication;
 import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.extensions.events.NewProjectCreatedListener;
+import com.google.gerrit.extensions.events.ProjectDeletedListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -32,6 +33,9 @@ class ReplicationModule extends AbstractModule {
         .to(ReplicationQueue.class);
 
     DynamicSet.bind(binder(), NewProjectCreatedListener.class)
+        .to(ReplicationQueue.class);
+
+    DynamicSet.bind(binder(), ProjectDeletedListener.class)
         .to(ReplicationQueue.class);
 
     bind(OnStartStop.class).in(Scopes.SINGLETON);
