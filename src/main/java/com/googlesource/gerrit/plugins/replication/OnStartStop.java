GerritCodeReview/plugins_replication

Show the replication status after running the replication command
The actual replication work is done asynchronously and it is split
into many subtasks, this patch adds the functionality that traces
every subtask execution, once the task is finished, we can get
replication status of all subtasks. This makes it possible for users
to start the replication ssh command, and wait to see information
about replication progress and errors in their consoles before the
command exits.

For the gerrit triggered replication of receiving a new patchset,
we can also add some features in the future, for example, send
stream events for a finished replication to a node.

Change-Id: I0fc64e93faa9157fc936379de29691282388ba26

src/main/java/com/googlesource/gerrit/plugins/replication/OnStartStop.java
@@ -46,7 +46,9 @@ class OnStartStop implements LifecycleListener {
 
     if (srvInfo.getState() == ServerInformation.State.STARTUP
         && queue.replicateAllOnPluginStart) {
-      pushAllFuture.set(pushAll.create(null).schedule(30, TimeUnit.SECONDS));
+      ReplicationState state =
+          new ReplicationState(ReplicationType.START_UP);
+      pushAllFuture.set(pushAll.create(null, state).schedule(30, TimeUnit.SECONDS));
     }
   }
 

src/main/java/com/googlesource/gerrit/plugins/replication/PushAll.java
@@ -30,23 +30,26 @@ import javax.annotation.Nullable;
 
 class PushAll implements Runnable {
   private static final Logger log = LoggerFactory.getLogger(PushAll.class);
+  private static final WrappedLogger wrappedLog = new WrappedLogger(log);
 
   interface Factory {
-    PushAll create(String urlMatch);
+    PushAll create(String urlMatch, ReplicationState state);
   }
 
   private final WorkQueue workQueue;
   private final ProjectCache projectCache;
   private final ReplicationQueue replication;
   private final String urlMatch;
+  private final ReplicationState state;
 
   @Inject
   PushAll(WorkQueue wq, ProjectCache projectCache, ReplicationQueue rq,
-      @Assisted @Nullable String urlMatch) {
+      @Assisted @Nullable String urlMatch, @Assisted ReplicationState state) {
     this.workQueue = wq;
     this.projectCache = projectCache;
     this.replication = rq;
     this.urlMatch = urlMatch;
+    this.state = state;
   }
 
   Future<?> schedule(long delay, TimeUnit unit) {
@@ -57,11 +60,12 @@ class PushAll implements Runnable {
   public void run() {
     try {
       for (Project.NameKey nameKey : projectCache.all()) {
-        replication.scheduleFullSync(nameKey, urlMatch);
+        replication.scheduleFullSync(nameKey, urlMatch, state);
       }
     } catch (Exception e) {
-      log.error("Cannot enumerate known projects", e);
+      wrappedLog.error("Cannot enumerate known projects", e, state);
     }
+    state.allTaskScheduled();
   }
 
   @Override

qtproject/qtqa-gerrit-plugin-replication

(cherry picked from commit 05f71912f4289480533773ffd05cb5cfcbe191bf)
Reviewed-by: Ismo Haataja <ismo.haataja@digia.com>
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/main/java/com/googlesource/gerrit/plugins/replication/OnStartStop.java
@@ -46,7 +46,9 @@ class OnStartStop implements LifecycleListener {
 
     if (srvInfo.getState() == ServerInformation.State.STARTUP
         && queue.replicateAllOnPluginStart) {
-      pushAllFuture.set(pushAll.create(null).schedule(30, TimeUnit.SECONDS));
+      ReplicationState state =
+          new ReplicationState(ReplicationType.STARTUP);
+      pushAllFuture.set(pushAll.create(null, state).schedule(30, TimeUnit.SECONDS));
     }
   }
 

src/main/java/com/googlesource/gerrit/plugins/replication/PushAll.java
@@ -30,23 +30,26 @@ import javax.annotation.Nullable;
 
 class PushAll implements Runnable {
   private static final Logger log = LoggerFactory.getLogger(PushAll.class);
+  private static final WrappedLogger wrappedLog = new WrappedLogger(log);
 
   interface Factory {
-    PushAll create(String urlMatch);
+    PushAll create(String urlMatch, ReplicationState state);
   }
 
   private final WorkQueue workQueue;
   private final ProjectCache projectCache;
   private final ReplicationQueue replication;
   private final String urlMatch;
+  private final ReplicationState state;
 
   @Inject
   PushAll(WorkQueue wq, ProjectCache projectCache, ReplicationQueue rq,
-      @Assisted @Nullable String urlMatch) {
+      @Assisted @Nullable String urlMatch, @Assisted ReplicationState state) {
     this.workQueue = wq;
     this.projectCache = projectCache;
     this.replication = rq;
     this.urlMatch = urlMatch;
+    this.state = state;
   }
 
   Future<?> schedule(long delay, TimeUnit unit) {
@@ -57,11 +60,12 @@ class PushAll implements Runnable {
   public void run() {
     try {
       for (Project.NameKey nameKey : projectCache.all()) {
-        replication.scheduleFullSync(nameKey, urlMatch);
+        replication.scheduleFullSync(nameKey, urlMatch, state);
       }
     } catch (Exception e) {
-      log.error("Cannot enumerate known projects", e);
+      wrappedLog.error("Cannot enumerate known projects", e, state);
     }
+    state.markAllPushTasksScheduled();
   }
 
   @Override